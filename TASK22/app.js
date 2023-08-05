fdcec81ef9b1b6d52a973733f148ef86e547cebc
Lakshmi Deepthi Koncha <deepureddy837@gmail.com>
2023-08-05 22:24:48 +0530

    Revert "first commit"
    This reverts commit 58637d66317754c2a2654ab9a22674a99856324c.

--- TASK22/app.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-
-var app=express()
-app.get('/',function(request,response){
-    response.send('Deepu');
-})
-app.listen(2003,function(){
-    console.log('Server started');
-})
--- TASK31/app.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-
-var app=express()
-app.get('/',function(req,res){
-    res.end("Deepu")
-})
-app.listen(2003)
